f402bf814b1feaf16213c80c17b21566590e09e1
gumaper <gumaper@outlook.com>
2019-04-15 14:22:30 -0400

    Exercicios do 1040 ao 1045

---
+++ Beginner/URI1042/URI1042/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace URI1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            string[] s = Console.ReadLine().Split(' ');
+            a = int.Parse(s[0]);
+            b = int.Parse(s[1]);
+            c = int.Parse(s[2]);
+
+            if (a < b && a < c && b < c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+            }
+            else if (a < b && a < c && b > c)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+            }
+            else if (a > b && b < c && a < c)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+                Console.WriteLine(c);
+            }
+            else if (a > b && b < c && a > c)
+            {
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+            }
+            else if (a < b && b > c && a > c)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+            }
+            else if (a > b && b > c && a > c)
+            {
+                Console.WriteLine(c);
+                Console.WriteLine(b);
+                Console.WriteLine(a);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
+
---
+++ Beginner/URI1043/URI1043/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            string[] values = Console.ReadLine().Split();
+            a = double.Parse(values[0], CultureInfo.InvariantCulture);
+            b = double.Parse(values[1], CultureInfo.InvariantCulture);
+            c = double.Parse(values[2], CultureInfo.InvariantCulture);
+
+            if (a < (b + c) && c < (a + b) && b < (a + c))
+            {
+                Console.WriteLine("Perimetro = " + (a + b + c).ToString("0.0", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("Area = " + (((a + b) * c) / 2).ToString("0.0", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
